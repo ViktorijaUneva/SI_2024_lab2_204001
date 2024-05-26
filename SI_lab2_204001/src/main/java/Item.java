2f978286f3f4aadc1ea77dafa5e7295cc1109bf4
Viktorija Uneva <unevaviktorija@gmail.com>
2024-05-26 21:33:01 +0200

    finished second lab

---
+++ SI_lab2_204001/src/main/java/Item.java
@@ -0,0 +1,48 @@
+package SI_lab2_204001.src.main.java;
+
+public class Item {
+    String name;
+    String barcode; 
+    int price;
+    float discount;
+
+    public Item(String name, String code, int price, float discount) {
+        this.name = name;
+        this.barcode = code;
+        this.price = price;
+        this.discount = discount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public float getDiscount() {
+        return discount;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBarcode(String code) {
+        this.barcode = code;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
+}
+
